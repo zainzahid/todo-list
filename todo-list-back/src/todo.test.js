4d4cdbaf34847a4af36dcc969acd73b1c76eb1a8
Zain <zainzahid99@gmail.com>
2021-10-26 05:00:31 +0500

    Tests for checking valid GET and POST Endpoints

--- todo-list-back/src/todo.test.js
+++ todo-list-back/src/todo.test.js
@@ -1,8 +1,22 @@
+const request = require('supertest')
+let app = require('./app')
+
 describe('todo-list API test suite', () => {
     it('should add todo task, POST', () => {
+        return request(app)
+        .post('/todo')
+        .send({
+            test: 'testing for POST',
+        })
+        .expect('Content-Type', /json/)
+        .expect(201)
     })
 
-    it('should return todo-list array', () => {
+    it('should return todo-list array, GET', () => {
+        return request(app)
+        .get('/todo')
+        .expect('Content-Type', /json/)
+        .expect(200)
     })
 
 })
