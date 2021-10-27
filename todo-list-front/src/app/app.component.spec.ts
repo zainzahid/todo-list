9a3f37dc97478cbad8676bef3c419364f534dc50
Zain <zainzahid99@gmail.com>
2021-10-28 03:57:18 +0500

    initial test for expected html elements

--- todo-list-front/src/app/app.component.spec.ts
+++ todo-list-front/src/app/app.component.spec.ts
@@ -15,4 +15,23 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('component should have the input to take todo_task', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const inputElement = fixture.nativeElement.querySelector('#todo')
+    expect(inputElement instanceof HTMLInputElement).toBeTruthy();
+  });
+
+  it('component should have the button to submit/add todo_task', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const inputElement = fixture.nativeElement.querySelector('#submit')
+    expect(inputElement instanceof HTMLButtonElement).toBeTruthy();
+  });
+
+  it('component should have the ul to display the added todo_tasks', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const inputElement = fixture.nativeElement.querySelector('#list')
+    expect(inputElement instanceof HTMLUListElement).toBeTruthy();
+  });
+
 });
