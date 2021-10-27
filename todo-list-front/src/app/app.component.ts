a51100ac4ae37e12fae764cd94dac17c254166b4
Zain <zainzahid99@gmail.com>
2021-10-28 04:33:11 +0500

    refactor ts code to add tasks in array

--- todo-list-front/src/app/app.component.ts
+++ todo-list-front/src/app/app.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newTask: string = '';
   tasks: string[] = [];
+  invalid: boolean = false;
+
+  addTask() {
+    if(!this.newTask){
+      this.invalid = true;
+      return;
+    }
+    this.invalid = false;
+    this.tasks.push(this.newTask);
+    this.newTask = '';
+  }
 }
